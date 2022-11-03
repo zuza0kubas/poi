091b82c390314bb36c55e26694959ef4013e7037
zuza0kubas <113593908+zuza0kubas@users.noreply.github.com>
2022-11-03 14:06:06 +0100

    Add files via upload

---
+++ nastepna-lekcja-java2.js
@@ -0,0 +1,7 @@
+
+ function display() {
+            var napis = document.getElementById("login").value;
+            document.getElementById("testy1").textContent = napis;
+            var napis2 = document.getElementById("haslo").value;
+            document.getElementById("testy2").textContent = napis2;
+        }
