83fc491c81a70ade41bc4a3525bf6c9a00902402
zuza0kubas <113593908+zuza0kubas@users.noreply.github.com>
2022-11-16 10:09:53 +0100

    Add files via upload

---
+++ zadZ.js
@@ -0,0 +1,14 @@
+    function wyswietl(){
+        var liczba = document.getElementById("numer").value;
+
+        for(var i=1; i <= liczba; i++){
+            document.write("*")
+                }
+            }
+             function wyswietl(){
+        var liczba = document.getElementById("numer").value;
+
+        for(var i=1; i <= liczba; i++){
+            document.write("*")
+                }
+            }
